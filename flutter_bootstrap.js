a9987d338208857ca77c32195726bf412f860716
shiva45678 <157795689+shiva45678@users.noreply.github.com>
2026-04-17 01:15:08 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2739027007"
+    serviceWorkerVersion: "1666563414"
   }
 });
